7f6382cdde2399384d61622eff163bbd81d742a7
siyasjr <siyasjr@gmail.com>
2025-08-26 12:25:20 +0530

    updated

--- types/types.ts
+++ types/types.ts
@@ -1,18 +1,19 @@
 export interface Movie {
   id: number;
   title: string;
-  name?: string;              // some endpoints use 'name'
+  name?: string;
   poster_path: string | null;
   backdrop_path: string | null;
-  release_date?: string;
-  first_air_date?: string;
-  vote_average: number;
+  first_air_date: string;
+  vote_average:number;
   overview: string;
+
 }
 
 export interface PaginatedResponse<T> {
   page: number;
   results: T[];
-  total_pages: number;
+  total_pages:number;
   total_results: number;
+
 }
