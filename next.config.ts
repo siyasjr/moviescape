432b303c4fdb2bb095b6aa1d850c4fab18c34738
siyasjr <siyasjr@gmail.com>
2025-09-22 00:08:29 +0530

    Added images.domains

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["image.tmdb.org"], // allow TMDB images
+  },
 };
 
 export default nextConfig;
